429ba1341291431b5672c3d0e2b8b6c1dce6fb87
ryusei-tsune <rt1226.tennis@gmail.com>
2021-10-15 14:10:24 +0900

    get book information

--- server-middleware/api/routerIndex.js
+++ server-middleware/api/routerIndex.js
@@ -7,27 +7,61 @@ const puppeteer = require('puppeteer');
 router.get('/scraping', async (req, res, next) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    var url = "https://webcat.lib.okayama-u.ac.jp/opac/volume/1293409?category-book=1&target=local"
+    //var url = "https://webcat.lib.okayama-u.ac.jp/opac/volume/1293409?category-book=1&target=local"
+    //var url = "https://webcat.lib.okayama-u.ac.jp/opac/search?q=%E5%AB%8C%E3%82%8F%E3%82%8C%E3%82%8B%E5%8B%87%E6%B0%97"
+    var url = "https://webcat.lib.okayama-u.ac.jp/opac/search?q=Julia"
     await page.goto(url, {
-        waitUntil: "networkidle2",
+        waitUntil: "networkidle0",
     });
-    
-    var resultText = [];
+
     var resultArray = await page.evaluate(() => {
         const datalist = [];
-        const resultlist = document.getElementsByTagName('p');
-        Array.prototype.forEach.call(resultlist, (element) => {
-            datalist.push(element.innerText);
+        const result = document.getElementById('resultCards');
+        const resultlist = result.children;
+        Array.from(resultlist).forEach((book) => {
+            var eachBook = book.getElementsByClassName('c_searchCard_topWrap')[0];
+            var elements = eachBook.getElementsByClassName('informationArea')[0].children;
+            var url = "";
+            var bookName = ""
+            var author = "";
+            var publisher = "";
+            var existing = "";
+            Array.from(elements).forEach((element, index) => {
+                switch (index) {
+                    case 0:
+                        var book = element.getElementsByTagName('a')[0]
+                        url = book.getAttribute('href');
+                        bookName = book.innerText;
+                        break;
+                    case 1:
+                        var auth = element.getElementsByTagName('span');
+                        Array.from(auth).forEach((person) => {
+                            author = person.innerText;
+                        })
+                        break;
+                    case 2:
+                        publisher = element.getElementsByTagName('span')[0].innerText;
+                        break;
+                    case 5:
+                        existing = element.getElementsByTagName('span')[0].innerText;
+                        break;
+                    default:
+                        break;
+                };
+            });
+            datalist.push({URL: url, bookname: bookName, Author: author, Publisher: publisher, Existing: existing})
         });
+        // const resultlist = document.getElementsByTagName('span');
+        // Array.prototype.forEach.call(resultlist, (element) => {
+        //     if ((element.innerText == "在庫中") || (element.innerText.includes("貸出中"))) datalist.push(element.innerText);
+        // });
         return datalist;
-    })
+    });
     console.log(resultArray);
-    // let resultSelectors = await page.$('p');
-    //  for (let i = 0; i < resultArray.length; i++) {
-    //      console.log(resultArray[i])
-    //  }
-    // console.log(resultText)
-    res.status(200).json({ ok: true, statusText: 'ok.', body: resultArray});
+    // const resultSelectors = await page.$('body');
+    // const html = await page.evaluate(body => body.innerHTML, resultSelectors);
+    // console.log(html)
+    res.status(200).json({ ok: true, statusText: 'ok.', body: resultArray });
     await browser.close();
 })
 router.get('/matsumoto', async (req, res, next) => {
