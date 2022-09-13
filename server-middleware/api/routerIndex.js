7226127b130e79bdecd9454888c2acb69e415cdc
ryusei-tsune <rt1226.tennis@gmail.com>
2022-09-13 12:35:20 +0900

    パッケージの更新，余分なAPIを削除

--- server-middleware/api/routerIndex.js
+++ server-middleware/api/routerIndex.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const puppeteer = require('puppeteer');
 
 router.post('/scraping', async (req, res, next) => {
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     //let url = "https://webcat.lib.okayama-u.ac.jp/opac/search?q=Julia";
     let url = "https://webcat.lib.okayama-u.ac.jp/opac/search?q=" + req.body.name;
@@ -56,44 +56,6 @@ router.post('/scraping', async (req, res, next) => {
     });
     console.log(resultArray);
     res.status(200).json(resultArray);
-    await browser.close();
-})
-router.post('/detail-information', async (req, res, next) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    //let url = "https://webcat.lib.okayama-u.ac.jp/opac/search?q=Julia";
-    let url = "https://webcat.lib.okayama-u.ac.jp" + req.body.url;
-    console.log(url)
-    await page.goto(url, {
-        waitUntil: "networkidle0",
-    });
-    let resultInfo = await page.evaluate(() => {
-        const datalist = [];
-        let i = 0;
-        while (1) {
-            const result = document.getElementById("lid_volume_td_0_st_" + i);
-            if (!result) {
-                break;
-            }
-            const existing = result.getElementsByTagName('p')[0].innerText;
-            if (existing == "在庫中") {
-                const floorResult = document.getElementById("lid_volume_td_0_lo_" + i);
-                const locationResult = document.getElementById("lid_volume_td_0_ca_" + i);
-
-                const floor = floorResult.getElementsByTagName('a')[0].innerText;
-                const locations = locationResult.getElementsByTagName('li');//3つ値が存在
-                let location = ""
-                for (let j = 0; j < locations.length; j++) {
-                    location += locations[j].innerText + "/"
-                }
-                datalist.push({ Floor: floor, Location: location })
-            }
-            i++;
-        }
-        return datalist;
-    });
-    console.log(resultInfo[0]);
-    res.status(200).json(resultInfo[0]);
     await browser.close();
 })
 
